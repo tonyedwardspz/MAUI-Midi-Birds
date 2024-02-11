b241d06a1ed0a229dfd40a434c8efd4fc8f43bd0
Tony Edwards <tony.edwards@gmail.com>
2024-02-11 11:05:32 +0000

    Adds MVVM

---
+++ MauiMidiBirds/ViewModels/LandingViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiMidiBirds.ViewModels;
+
+public partial class LandingViewModel : ObservableObject
+{
+
+	[ObservableProperty] private string ?latestKey;
+
+	public LandingViewModel()
+	{
+
+	}
+
+	[RelayCommand]
+	void ReadKey()
+	{
+		LatestKey = "C";
+	}
+}
+
---
+++ MauiMidiBirds/Views/LandingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiMidiBirds.Views;
+
+using MauiMidiBirds.ViewModels;
+
+public partial class LandingView : ContentPage
+{
+	public LandingView()
+	{
+		InitializeComponent();
+		BindingContext = new LandingViewModel();
+	}
+}
