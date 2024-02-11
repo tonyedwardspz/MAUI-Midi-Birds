2925e57079f9b754c6f9d9bde0f3a68a275228bc
Tony Edwards <tony.edwards@gmail.com>
2024-02-11 11:45:07 +0000

    Connects controller to the application

--- MauiMidiBirds/ViewModels/LandingViewModel.cs
+++ MauiMidiBirds/ViewModels/LandingViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Commons.Music.Midi;
+using System.Diagnostics;
 
 namespace MauiMidiBirds.ViewModels;
 
@@ -8,16 +10,27 @@ public partial class LandingViewModel : ObservableObject
 {
 
 	[ObservableProperty] private string ?latestKey;
+	internal string controllerName = "APC Key 25";
 
-	public LandingViewModel()
+
+    public LandingViewModel()
 	{
 		LatestKey = "C";
-	}
 
-	[RelayCommand]
-	void ReadKey()
-	{
-		LatestKey = "B";
-	}
+        
+        var access = MidiAccessManager.Default;
+		var inputs = access.Inputs;
+		var deviceNumber = inputs.Where(i => i.Name == controllerName).FirstOrDefault();
+		var input = access.OpenInputAsync(deviceNumber.Id).Result;
+		input.MessageReceived += Input_MessageRecieved;
+		
+    }
+
+    private void Input_MessageRecieved(object? sender, MidiReceivedEventArgs e)
+    {
+		Debug.WriteLine("Message Recieved");
+		Debug.WriteLine($"{e.Data[0].ToString()} - {e.Data[1].ToString()} - {e.Data[2].ToString()}");
+		LatestKey = e.Data[1].ToString();
+    }
 }
 
