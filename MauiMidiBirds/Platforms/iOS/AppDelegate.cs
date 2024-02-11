f443d4d6c1e92c74cb03c75579cf0e9fc04fd480
Tony Edwards <tony.edwards@gmail.com>
2024-02-11 11:05:17 +0000

    Init - Building Mac App

---
+++ MauiMidiBirds/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiMidiBirds;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
